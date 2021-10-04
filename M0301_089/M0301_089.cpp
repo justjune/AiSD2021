f3d8249702aa3621914768b301bfcc13aae66739
romanov_maksim <88290162+Makson4ik001@users.noreply.github.com>
2021-10-04 12:20:13 +0500

    Романов: связный список M0301_089

---
+++ M0301_089/M0301_089.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+class List
+{
+private:
+    Node* head; //"голова" связанного списка
+public:
+    List() //конструктор класса без параметров
+    {
+        head = NULL; //первого элемента пока нет
+    }
+    //метод, добавляющий новый узел в список
+    void addNode(int d)
+    {
+        Node* nd = new Node; //динамически создаем новый узел
+        nd->data = d;        //задаем узлу данные
+        nd->next = NULL;     //новый узел в конце, поэтому NULL
+        if (head == NULL)     //если создаем первый узел
+            head = nd;
+        else                 //если узел уже не первый
+        {
+            Node* current = head;
+            //ищем в цикле предшествующий последнему узел
+            while (current->next != NULL)
+                current = current->next;
+            //предшествующий указывает на последний
+            current->next = nd;
+        }
+    }
+    //метод, выводящий связанный список на экран
+    void printList()
+    {
+        Node* current = head;
+        while (current != NULL)
+        {
+            cout << current->data << endl;
+            current = current->next;
+        }
+    }
+};
+int main()
+{
+    List myList;
+    myList.addNode(5);
+    myList.addNode(11);
+    myList.addNode(27);
+    myList.printList();
+    return 0;
+}
